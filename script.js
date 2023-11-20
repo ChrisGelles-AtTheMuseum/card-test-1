a0fa97410eee101c28ca077401144b8c57ffede6
ChrisGelles-AtTheMuseum <139920213+ChrisGelles-AtTheMuseum@users.noreply.github.com>
2023-11-20 16:04:56 -0500

    Main index and script

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var animation1 = lottie.loadAnimation({
+        container: document.getElementById('lottie1'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: 'top.json' // Update this path
+    });
+
+    var animation2 = lottie.loadAnimation({
+        container: document.getElementById('lottie2'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        path: 'bottom.json' // Update this path
+    });
+
+    document.getElementById('container').addEventListener('click', function() {
+        animation1.play();
+        animation2.play();
+    });
+});
