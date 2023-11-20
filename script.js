971bdbb9fa5da9d1e361bba625f46c8446c76d5a
ChrisGelles-AtTheMuseum <139920213+ChrisGelles-AtTheMuseum@users.noreply.github.com>
2023-11-20 17:25:51 -0500

    Attempting to add audio play in the script again.

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function() {
         path: 'top.json' // Update this path
     });
 
+    var audio = document.getElementById('myAudio'); // Get the audio element
+
     // Bottom Lottie (Loops)
     var animation2 = lottie.loadAnimation({
         container: document.getElementById('lottie2'),
@@ -17,14 +19,19 @@ document.addEventListener("DOMContentLoaded", function() {
         path: 'bottom.json' // Update this path
     });
 
-    // Play top Lottie once when clicked and then remove the event listener
-    var lottie1Container = document.getElementById('lottie1');
+    // Function to play top Lottie and audio once
     var playAnimation1 = function() {
         animation1.goToAndPlay(0, true);
-        //audio.play(); // Play the audio
-        lottie1Container.removeEventListener('click', playAnimation1);
+        audio.play(); // Play the audio
     };
-    lottie1Container.addEventListener('click', playAnimation1);
+
+    // Event listener for the top Lottie
+    var lottie1Container = document.getElementById('lottie1');
+    lottie1Container.addEventListener('click', function() {
+        playAnimation1();
+        // Remove the event listener after the first click
+        lottie1Container.removeEventListener('click', playAnimation1);
+    });
 
     // Play/Pause bottom Lottie on click
     document.getElementById('lottie2').addEventListener('click', function() {
