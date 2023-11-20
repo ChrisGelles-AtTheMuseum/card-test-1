4ada154ee292ac5b8586c9a64cf9f9018db4d312
ChrisGelles-AtTheMuseum <139920213+ChrisGelles-AtTheMuseum@users.noreply.github.com>
2023-11-20 16:15:24 -0500

    Play top once, loop bottom.

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Top Lottie (Plays Once)
     var animation1 = lottie.loadAnimation({
         container: document.getElementById('lottie1'),
         renderer: 'svg',
         loop: false,
         autoplay: false,
-        path: 'top.json' // Update this path
+        path: 'path_to_your_first_animation.json' // Update this path
     });
 
+    // Bottom Lottie (Loops)
     var animation2 = lottie.loadAnimation({
         container: document.getElementById('lottie2'),
         renderer: 'svg',
-        loop: false,
+        loop: true,
         autoplay: false,
-        path: 'bottom.json' // Update this path
+        path: 'path_to_your_second_animation.json' // Update this path
     });
 
-    document.getElementById('container').addEventListener('click', function() {
-        animation2.play();
-        animation1.play();
+    // Play top Lottie once when clicked
+    document.getElementById('lottie1').addEventListener('click', function() {
+        animation1.goToAndPlay(0, true);
+    });
+
+    // Play/Pause bottom Lottie on click
+    document.getElementById('lottie2').addEventListener('click', function() {
+        if (animation2.isPaused) {
+            animation2.play();
+        } else {
+            animation2.pause();
+        }
     });
 });
